145c45e7650e2a413998c2abaf0e901aec3cd812
João Rafael <joaoraf@me.com>
2023-03-27 13:50:01 -0300

    Add support for importing images as a dictionary instead of a list.

--- code/soil.py
+++ code/soil.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from pytmx.util_pygame import load_pygame
+from support import *
 
 class SoilTile(pygame.sprite.Sprite):
 	def __init__(self, pos, surf, groups):
@@ -18,6 +19,7 @@ class SoilLayer:
 
 		# graphics
 		self.soil_surf = pygame.image.load('../graphics/soil/o.png')
+		self.soil_surfs = import_folder_dict('../graphics/soil/')
 
 		self.create_soil_grid()
 		self.create_hit_rects()
@@ -55,7 +57,42 @@ class SoilLayer:
 		for index_row, row in enumerate(self.grid):
 			for index_col, cell in enumerate(row):
 				if 'X' in cell:
+					
+					# tile options 
+					t = 'X' in self.grid[index_row - 1][index_col]
+					b = 'X' in self.grid[index_row + 1][index_col]
+					r = 'X' in row[index_col + 1]
+					l = 'X' in row[index_col - 1]
+
+					tile_type = 'o'
+
+					# all sides
+					if all((t,r,b,l)): tile_type = 'x'
+
+					# horizontal tiles only
+					if l and not any((t,r,b)): tile_type = 'r'
+					if r and not any((t,l,b)): tile_type = 'l'
+					if r and l and not any((t,b)): tile_type = 'lr'
+
+					# vertical only 
+					if t and not any((r,l,b)): tile_type = 'b'
+					if b and not any((r,l,t)): tile_type = 't'
+					if b and t and not any((r,l)): tile_type = 'tb'
+
+					# corners 
+					if l and b and not any((t,r)): tile_type = 'tr'
+					if r and b and not any((t,l)): tile_type = 'tl'
+					if l and t and not any((b,r)): tile_type = 'br'
+					if r and t and not any((b,l)): tile_type = 'bl'
+
+					# T shapes
+					if all((t,b,r)) and not l: tile_type = 'tbr'
+					if all((t,b,l)) and not r: tile_type = 'tbl'
+					if all((l,r,t)) and not b: tile_type = 'lrb'
+					if all((l,r,b)) and not t: tile_type = 'lrt'
+
+
 					SoilTile(
 						pos = (index_col * TILE_SIZE,index_row * TILE_SIZE), 
-						surf = self.soil_surf, 
+						surf = self.soil_surfs[tile_type], 
 						groups = [self.all_sprites, self.soil_sprites])
