de23e03632d42bb64101c92d63094cede2f00dc6
João Rafael <joaoraf@me.com>
2023-03-27 13:42:43 -0300

    Add Overlay class to display tools and seeds icons on screen.

--- code/level.py
+++ code/level.py
@@ -1,6 +1,7 @@
 import pygame 
 from settings import *
 from player import Player
+from overlay import Overlay
 
 class Level:
 	def __init__(self):
@@ -12,6 +13,7 @@ class Level:
 		self.all_sprites = pygame.sprite.Group()
 
 		self.setup()
+		self.overlay = Overlay(self.player)
 
 	def setup(self):
 		self.player = Player((640,360), self.all_sprites)
@@ -20,3 +22,5 @@ class Level:
 		self.display_surface.fill('black')
 		self.all_sprites.draw(self.display_surface)
 		self.all_sprites.update(dt)
+
+		self.overlay.display()
