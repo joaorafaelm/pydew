4dd8e958ba892d07583fdb9094d2d479a8e170c8
João Rafael <joaoraf@me.com>
2023-03-27 13:37:13 -0300

    Update animation and input methods in Player class.

--- code/player.py
+++ code/player.py
@@ -7,7 +7,7 @@ class Player(pygame.sprite.Sprite):
 		super().__init__(group)
 
 		self.import_assets()
-		self.status = 'left_water'
+		self.status = 'down_idle'
 		self.frame_index = 0
 
 		# general setup
@@ -30,22 +30,41 @@ class Player(pygame.sprite.Sprite):
 			full_path = '../graphics/character/' + animation
 			self.animations[animation] = import_folder(full_path)
 
+	def animate(self,dt):
+		self.frame_index += 4 * dt
+		if self.frame_index >= len(self.animations[self.status]):
+			self.frame_index = 0
+
+		self.image = self.animations[self.status][int(self.frame_index)]
+
 	def input(self):
 		keys = pygame.key.get_pressed()
 
 		if keys[pygame.K_UP]:
 			self.direction.y = -1
+			self.status = 'up'
 		elif keys[pygame.K_DOWN]:
 			self.direction.y = 1
+			self.status = 'down'
 		else:
 			self.direction.y = 0
 
 		if keys[pygame.K_RIGHT]:
 			self.direction.x = 1
+			self.status = 'right'
 		elif keys[pygame.K_LEFT]:
 			self.direction.x = -1
+			self.status = 'left'
 		else:
 			self.direction.x = 0
+
+	def get_status(self):
+		
+		# idle
+		if self.direction.magnitude() == 0:
+			self.status = self.status.split('_')[0] + '_idle'
+
+		# tool use
 
 	def move(self,dt):
 
@@ -63,4 +82,7 @@ class Player(pygame.sprite.Sprite):
 
 	def update(self, dt):
 		self.input()
+		self.get_status()
+
 		self.move(dt)
+		self.animate(dt)
