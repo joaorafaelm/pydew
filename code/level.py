52c626578135eb8e341624435f077019dbbc387a
João Rafael <joaoraf@me.com>
2023-03-27 13:48:14 -0300

    Add soil layer to player and enable soil tile creation on tool hit.

--- code/level.py
+++ code/level.py
@@ -6,6 +6,7 @@ from sprites import Generic, Water, WildFlower, Tree, Interaction
 from pytmx.util_pygame import load_pygame
 from support import *
 from transition import Transition
+from soil import SoilLayer
 
 class Level:
 	def __init__(self):
@@ -19,6 +20,7 @@ class Level:
 		self.tree_sprites = pygame.sprite.Group()
 		self.interaction_sprites = pygame.sprite.Group()
 
+		self.soil_layer = SoilLayer(self.all_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
@@ -69,7 +71,8 @@ class Level:
 					group = self.all_sprites, 
 					collision_sprites = self.collision_sprites,
 					tree_sprites = self.tree_sprites,
-					interaction = self.interaction_sprites)
+					interaction = self.interaction_sprites,
+					soil_layer = self.soil_layer)
 			
 			if obj.name == 'Bed':
 				Interaction((obj.x,obj.y), (obj.width,obj.height), self.interaction_sprites, obj.name)
--- code/player.py
+++ code/player.py
@@ -4,7 +4,7 @@ from support import *
 from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-	def __init__(self, pos, group, collision_sprites, tree_sprites, interaction):
+	def __init__(self, pos, group, collision_sprites, tree_sprites, interaction, soil_layer):
 		super().__init__(group)
 
 		self.import_assets()
@@ -55,11 +55,11 @@ class Player(pygame.sprite.Sprite):
 		self.tree_sprites = tree_sprites
 		self.interaction = interaction
 		self.sleep = False
+		self.soil_layer = soil_layer
 
 	def use_tool(self):
-		print('tool use')
 		if self.selected_tool == 'hoe':
-			pass
+			self.soil_layer.get_hit(self.target_pos)
 		
 		if self.selected_tool == 'axe':
 			for tree in self.tree_sprites.sprites():
