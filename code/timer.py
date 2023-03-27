f6aa1355759e8948d7f3d0cb761dc6e9c4ed231e
João Rafael <joaoraf@me.com>
2023-03-27 13:46:06 -0300

    Added tree sprites and tool interaction functionality to the player.

--- code/timer.py
+++ code/timer.py
@@ -18,6 +18,6 @@ class Timer:
 	def update(self):
 		current_time = pygame.time.get_ticks()
 		if current_time - self.start_time >= self.duration:
-			self.deactivate()
-			if self.func:
+			if self.func and self.start_time != 0:
 				self.func()
+			self.deactivate()
