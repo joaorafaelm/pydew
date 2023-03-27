49553f440e5d6f468a62bc5e9f525f693318f08f
João Rafael <joaoraf@me.com>
2023-03-27 13:50:56 -0300

    Add plant growth and planting functionality to soil layer.

--- code/level.py
+++ code/level.py
@@ -22,7 +22,7 @@ class Level:
 		self.tree_sprites = pygame.sprite.Group()
 		self.interaction_sprites = pygame.sprite.Group()
 
-		self.soil_layer = SoilLayer(self.all_sprites)
+		self.soil_layer = SoilLayer(self.all_sprites, self.collision_sprites)
 		self.setup()
 		self.overlay = Overlay(self.player)
 		self.transition = Transition(self.reset, self.player)
@@ -96,6 +96,8 @@ class Level:
 		self.player.item_inventory[item] += 1
 
 	def reset(self):
+		# plants
+		self.soil_layer.update_plants()
 
 		# soil
 		self.soil_layer.remove_water()
--- code/player.py
+++ code/player.py
@@ -75,7 +75,7 @@ class Player(pygame.sprite.Sprite):
 
 	def use_seed(self):
 
-		pass
+		self.soil_layer.plant_seed(self.target_pos, self.selected_seed)
 
 	def import_assets(self):
 		self.animations = {'up': [],'down': [],'left': [],'right': [],
