879752126ea50d68a2b7cefafb5754a972a3c306
João Rafael <joaoraf@me.com>
2023-03-27 13:47:46 -0300

    Add player inventory management to Tree class.

--- code/level.py
+++ code/level.py
@@ -43,7 +43,12 @@ class Level:
 
 		# trees 
 		for obj in tmx_data.get_layer_by_name('Trees'):
-			Tree((obj.x, obj.y), obj.image, [self.all_sprites, self.collision_sprites, self.tree_sprites], obj.name)
+			Tree(
+				pos = (obj.x, obj.y), 
+				surf = obj.image, 
+				groups = [self.all_sprites, self.collision_sprites, self.tree_sprites], 
+				name = obj.name,
+				player_add = self.player_add)
 
 		# wildflowers 
 		for obj in tmx_data.get_layer_by_name('Decoration'):
@@ -66,6 +71,10 @@ class Level:
 			surf = pygame.image.load('../graphics/world/ground.png').convert_alpha(),
 			groups = self.all_sprites,
 			z = LAYERS['ground'])
+
+	def player_add(self,item):
+
+		self.player.item_inventory[item] += 1
 
 	def run(self,dt):
 		self.display_surface.fill('black')
--- code/sprites.py
+++ code/sprites.py
@@ -57,7 +57,7 @@ class Particle(Generic):
 			self.kill()
 
 class Tree(Generic):
-	def __init__(self, pos, surf, groups, name):
+	def __init__(self, pos, surf, groups, name, player_add):
 		super().__init__(pos, surf, groups)
 
 		# tree attributes
@@ -73,6 +73,8 @@ class Tree(Generic):
 		self.apple_sprites = pygame.sprite.Group()
 		self.create_fruit()
 
+		self.player_add = player_add
+
 	def damage(self):
 		
 		# damaging the tree
@@ -86,6 +88,7 @@ class Tree(Generic):
 				surf = random_apple.image, 
 				groups = self.groups()[0], 
 				z = LAYERS['fruit'])
+			self.player_add('apple')
 			random_apple.kill()
 
 	def check_death(self):
@@ -95,6 +98,7 @@ class Tree(Generic):
 			self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
 			self.hitbox = self.rect.copy().inflate(-10,-self.rect.height * 0.6)
 			self.alive = False
+			self.player_add('wood')
 
 	def update(self,dt):
 		if self.alive:
