807dbe1879d27e428db98c6401ad33940fe82550
João Rafael <joaoraf@me.com>
2023-03-27 13:41:19 -0300

    Add timer class and implement tool use timer in Player class.

--- code/player.py
+++ code/player.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from support import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
 	def __init__(self, pos, group):
@@ -18,6 +19,18 @@ class Player(pygame.sprite.Sprite):
 		self.direction = pygame.math.Vector2()
 		self.pos = pygame.math.Vector2(self.rect.center)
 		self.speed = 200
+
+		# timers 
+		self.timers = {
+			'tool use': Timer(350,self.use_tool)
+		}
+
+		# tools 
+		self.selected_tool = 'water'
+
+	def use_tool(self):
+
+		print(self.selected_tool)
 
 	def import_assets(self):
 		self.animations = {'up': [],'down': [],'left': [],'right': [],
@@ -40,23 +53,31 @@ class Player(pygame.sprite.Sprite):
 	def input(self):
 		keys = pygame.key.get_pressed()
 
-		if keys[pygame.K_UP]:
-			self.direction.y = -1
-			self.status = 'up'
-		elif keys[pygame.K_DOWN]:
-			self.direction.y = 1
-			self.status = 'down'
-		else:
-			self.direction.y = 0
+		if not self.timers['tool use'].active:
+			# directions 
+			if keys[pygame.K_UP]:
+				self.direction.y = -1
+				self.status = 'up'
+			elif keys[pygame.K_DOWN]:
+				self.direction.y = 1
+				self.status = 'down'
+			else:
+				self.direction.y = 0
 
-		if keys[pygame.K_RIGHT]:
-			self.direction.x = 1
-			self.status = 'right'
-		elif keys[pygame.K_LEFT]:
-			self.direction.x = -1
-			self.status = 'left'
-		else:
-			self.direction.x = 0
+			if keys[pygame.K_RIGHT]:
+				self.direction.x = 1
+				self.status = 'right'
+			elif keys[pygame.K_LEFT]:
+				self.direction.x = -1
+				self.status = 'left'
+			else:
+				self.direction.x = 0
+
+			# tool use
+			if keys[pygame.K_SPACE]:
+				self.timers['tool use'].activate()
+				self.direction = pygame.math.Vector2()
+				self.frame_index = 0
 
 	def get_status(self):
 		
@@ -65,6 +86,12 @@ class Player(pygame.sprite.Sprite):
 			self.status = self.status.split('_')[0] + '_idle'
 
 		# tool use
+		if self.timers['tool use'].active:
+			self.status = self.status.split('_')[0] + '_' + self.selected_tool
+
+	def update_timers(self):
+		for timer in self.timers.values():
+			timer.update()
 
 	def move(self,dt):
 
@@ -83,6 +110,7 @@ class Player(pygame.sprite.Sprite):
 	def update(self, dt):
 		self.input()
 		self.get_status()
+		self.update_timers()
 
 		self.move(dt)
 		self.animate(dt)
